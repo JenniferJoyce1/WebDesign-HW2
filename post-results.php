3428f8fd0afaf38baf47c83b7f05985257aaf476
JenniferJoyce1 <111783714+JenniferJoyce1@users.noreply.github.com>
2023-09-06 14:15:49 -0500

    Update post-results.php

--- post-results.php
+++ post-results.php
@@ -4,15 +4,16 @@ include "view-header.php";
 ?>
     <h1>Post results</h1>
 <?php
-if (isset($_POST['my-name'])) {
-  ?>
-  <p> the value sent is:</p>
-  <?php
-  echo $_POST['my-name'];
-} else {
-  ?>
-  <p> nothing posted</p>
-  <?php
-}
+echo getDisplay();
 include "view-footer.php";
+
+function getDisplay() {
+    if (isset($_POST['my-name'])) {
+  return "<p> the value sent is:</p>" . $_POST['my-name'];
+} else {
+     return "<p> nothing posted</p>";
+}
+}
+
+
 ?>
